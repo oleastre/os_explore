60053be88094e7857ec1e7124f4957f9f897a740
Olivier Samyn <olivier@26lights.com>
2016-04-12 21:33:14 +0200

    Add mapgen chat command that uses emerge_area.

--- init.lua
+++ init.lua
@@ -85,4 +85,46 @@ minetest.register_globalstep(function(dtime)
 end
 )
 
+minetest.register_chatcommand("mapgen", {
+  params = "radius, max_height",
+  description = "Generate map in a square box of size 2*radius up to max_height centered at your current position.",
+  func = function(name, params)
+    local found, _, s_radius, s_height = params:find("^%s*(%d+)%s*,%s*(-?%d+)%s*$")
+    if found == nil then
+      minetest.chat_send_player(name, "Usage: /mapgen radius, max_height")
+      return
+    end
+    local radius = tonumber(s_radius)
+    local max_height = tonumber(s_height)
 
+    if radius == 0 then radius = 1 end
+
+    local player = minetest.get_player_by_name(name)
+    local pos = player:getpos()
+    local start_pos = {
+      x = pos.x - radius,
+      y = pos.y,
+      z = pos.z - radius
+    }
+    local end_pos = {
+      x = pos.x + radius,
+      y = max_height,
+      z = pos.z + radius
+    }
+    local start_time = minetest.get_us_time()
+
+    minetest.emerge_area(start_pos, end_pos, function(blockpos, action, remaining)
+      local dt = math.floor((minetest.get_us_time() - start_time) / 1000)
+      local block = (blockpos.x * 16)..","..(blockpos.y * 16)..","..(blockpos.z * 16)
+      local info = "(mapgen-"..remaining.."-"..dt.."ms) "
+      if action==core.EMERGE_GENERATED then
+          minetest.chat_send_player(name, info.."Generated new block at "..block)
+        elseif (action==core.EMERGE_CANCELLED) or (action==core.EMERGE_ERRORED) then
+          minetest.chat_send_player(name, info.."Block at "..block.." did not emerge")
+        else
+          --minetest.chat_send_player(name, "(mapgen-"..remaining.."-"..dt.."s) Visited block at "..(blockpos.x)..","..(blockpos.y)..","..(blockpos.z))
+        end
+      end
+    )
+  end
+})
